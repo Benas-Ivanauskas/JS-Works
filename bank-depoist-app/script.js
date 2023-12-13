f4a08ef42d8914757ca79fd127d38563dc7ffb9b
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2023-12-13 17:54:13 +0200

    update

--- bank-depoist-app/script.js
+++ bank-depoist-app/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const account1 = {
+  owner: "Tom Gold",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jesica Shy",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Stewen Smith",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  pin: 3333,
+};
+
+const accounts = [account1, account2, account3];
