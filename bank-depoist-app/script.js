9bf59cd923255810fc930a0a91ae68fd5422bade
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2023-12-14 08:53:51 +0200

    update

--- bank-depoist-app/script.js
+++ bank-depoist-app/script.js
@@ -19,3 +19,45 @@ const account3 = {
 };
 
 const accounts = [account1, account2, account3];
+
+// ---------Nav---------
+const labelWelcome = document.querySelector(".welcome");
+const inputLogin = document.querySelector(".login_input--user");
+const inputPin = document.querySelector(".login_input--pin");
+const labelLoginBtn = document.querySelector(".login_btn");
+// --------Balance----------
+const balanceLabel = document.querySelector(".balance_laber");
+const balanceDate = document.querySelector(".balance_date");
+const balanceValue = document.querySelector(".balance_value");
+// ---------Movements---------
+const movementsTypeDeposit = document.querySelector(".movements_type--deposit");
+const movemetsValue = document.querySelector(".movements_value--value");
+const movementsTypeWithdrawal = document.querySelector(
+  ".movements_type--withdrawal"
+);
+const boxMovements = document.querySelector(".movements");
+// --------Summarry--------
+const summaryIn = document.querySelector(".summary_label--in");
+const summaryOut = document.querySelector(".summary_label--out");
+const summarySort = document.querySelector(".btn--sort");
+
+//pirma uzduotis, perkelti account1 movements duomenis is array i UI.
+const displayMovements = function (movements) {
+  account1.movements.forEach((movement, i) => {
+    const type = movement > 0 ? "deposit" : "withdrawal";
+    const html = `<div class="movements_row">
+    <p class="movements_type movements_type--${type}">${i + 1}  ${type}</p>
+    <p class="movements_value movements_value--value">${movement} Eur</p>
+  </div>`;
+    boxMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+displayMovements(account1.movements);
+
+const displayCalcBalance = function (acc) {
+  acc.balance = account1.movements.reduce((acc, mov) => acc + mov, 0);
+  balanceValue.textContent = `${acc.balance} Eur`;
+};
+
+displayCalcBalance(account1.movements);
