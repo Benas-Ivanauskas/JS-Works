5ed807ffdc96c01bc952a9ab0558d424410132ea
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2023-12-14 10:00:23 +0200

    update

--- bank-depoist-app/script.js
+++ bank-depoist-app/script.js
@@ -22,7 +22,7 @@ const accounts = [account1, account2, account3];
 
 // ---------Nav---------
 const labelWelcome = document.querySelector(".welcome");
-const inputLogin = document.querySelector(".login_input--user");
+const inputLoginUserName = document.querySelector(".login_input--user");
 const inputPin = document.querySelector(".login_input--pin");
 const labelLoginBtn = document.querySelector(".login_btn");
 // --------Balance----------
@@ -41,46 +41,75 @@ const summaryIn = document.querySelector(".summary_label--in");
 const summaryOut = document.querySelector(".summary_label--out");
 const summarySort = document.querySelector(".btn--sort");
 
-//pirma uzduotis, perkelti account1 movements duomenis is array i UI.
+//1. pirma uzduotis, perkelti account1 movements duomenis is array i UI.
 const displayMovements = function (movements) {
+  boxMovements.innerHTML = ""; // leido nuimti html irasytas reiksmes, del to klaidu turejau su forEach metodu
+
   account1.movements.forEach((movement, i) => {
     const type = movement > 0 ? "deposit" : "withdrawal";
     const html = `<div class="movements_row">
-    <p class="movements_type movements_type--${type}">${i + 1}  ${type}</p>
+    <p class="movements_type movements_type--${type}"> ${i + 1} ${type} </p>
     <p class="movements_value movements_value--value">${movement} Eur</p>
   </div>`;
+
     boxMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
 
 displayMovements(account1.movements);
 
-//Paskaiciuoti acc pinigu suma
-const displayCalcBalance = function (acc) {
+//2.Antra uzduotis, susiraseme viska i viena funkcija ir suskaiciuojame visus inestus pinigus tam tikruose laukeliuose
+const displayCalculationsMovements = function (acc) {
+  //Skaiciuojame balance suma
   acc.balance = account1.movements.reduce((acc, mov) => acc + mov, 0);
   balanceValue.textContent = `${acc.balance} Eur`;
-};
-displayCalcBalance(account1.movements);
 
-//Paskaiciuoti acc inestu pinigu suma
-const displayCalcSumIn = function (acc) {
+  //Skaiciuojame acc inestu pinigu suma In
   acc.sumIn = account1.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   summaryIn.textContent = `${acc.sumIn} Eur`;
-};
-displayCalcSumIn(account1.movements);
-//paskaiciuoti acc nusiimtu pinigu suma
 
-const displayCalcSumOut = function (acc) {
+  //paskaiciuoti acc nusiimtu pinigu suma
   acc.sumOut = account1.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   summaryOut.textContent = `${Math.abs(acc.sumOut)} Eur`;
 };
-displayCalcSumOut(account1.movements);
+displayCalculationsMovements(account1.movements);
 
+//3. Trecia uzduotis nustatytis siandienos data
 const displayCurrentDate = function () {
   balanceDate.textContent = `${new Date().toLocaleDateString()}`;
 };
 displayCurrentDate();
+
+//4. ketvirta uzduotis. Prisijungti su tam tikru Acc
+
+const createUserNames = function (accounts) {
+  accounts.forEach(
+    (acc) =>
+      (acc.username = acc.owner
+        .toLowerCase()
+        .split(" ")
+        .map(function (name) {
+          return name[0];
+        })
+        .join(""))
+  );
+};
+createUserNames(accounts);
+
+//login event handler
+
+//let kintamasis reikalingas outsides addeventlistener function. Kad butu galima transferint pinigus is kurio acc i kazkieno acc
+let currentAccount;
+
+labelLoginBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("LOGIN");
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUserName.value
+  );
+  console.log(currentAccount);
+});
