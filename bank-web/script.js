a895981dc864e091241a03e6e30c52099b471cad
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2023-12-29 12:47:48 +0200

    update

---
+++ bank-web/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const nav = document.querySelector(".nav");
+
+//NavBar Fade animation
+nav.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((element) => {
+      if (element !== link) element.style.opacity = 0.5;
+    });
+    logo.style.opacity = 0.5;
+  }
+});
+
+nav.addEventListener("mouseout", function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((element) => {
+      if (element !== link) element.style.opacity = 1;
+    });
+    logo.style.opacity = 1;
+  }
+});
