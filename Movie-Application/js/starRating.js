244be2df9de78c8a1b1a0b215c647a6e013310d6
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2024-01-18 09:39:08 +0200

    update starRatings.js

--- Movie-Application/js/starRating.js
+++ Movie-Application/js/starRating.js
@@ -37,3 +37,32 @@ export function starRatingUpdate(movieID) {
     });
   });
 }
+
+//Previous code just for star rating set colors
+// function starRatingUpdate() {
+//   let output = document.querySelector("#rating-output");
+//   let stars = document.querySelectorAll(".star");
+
+//   stars.forEach((star, index) => {
+//     star.addEventListener("click", function () {
+//       // Reset all stars to default color
+//       stars.forEach((s) => (s.style.color = "black"));
+
+//       // Set the color of clicked star and all stars before it
+//       for (let i = 0; i <= index; i++) {
+//         stars[i].style.color = "red";
+//       }
+//       if (index + 1 === 1) {
+//         output.innerText = "👎 Rating is: " + (index + 1) + "/5";
+//       } else if (index + 1 === 2) {
+//         output.innerText = "😟 Rating is: " + (index + 1) + "/5";
+//       } else if (index + 1 === 3) {
+//         output.innerText = "🙂 Rating is: " + (index + 1) + "/5";
+//       } else if (index + 1 === 4) {
+//         output.innerText = "😆 Rating is: " + (index + 1) + "/5";
+//       } else if (index + 1 === 5) {
+//         output.innerText = "😍 Rating is: " + (index + 1) + "/5";
+//       }
+//     });
+//   });
+// }
